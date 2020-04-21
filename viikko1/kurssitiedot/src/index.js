1eae04df5db5e96c6d8540afbf69af80d29636fa
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-04-21 10:37:21 +0300

    1.5

--- viikko1/kurssitiedot/src/index.js
+++ viikko1/kurssitiedot/src/index.js
@@ -3,21 +3,23 @@ import ReactDOM from 'react-dom'
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = (props) => {
     console.log(props)
@@ -63,9 +65,9 @@ const App = () => {
 
   return (
     <div>
-      <Header name={course} />
-      <Content parts = {parts}/>
-      <Total parts = {parts} />
+      <Header name={course.name} />
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts} />
       
      
     </div>
