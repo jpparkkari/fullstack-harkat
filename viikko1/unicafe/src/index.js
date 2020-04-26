36c1b3651574a6c17b410934c1ba90cff4af5082
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-04-26 10:59:48 +0300

    unicafe 1.10 ja app.js -> index.js

--- viikko1/unicafe/src/index.js
+++ viikko1/unicafe/src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td><td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  if (total === 0) {
+    return (
+      <div>
+        <h3>statistics</h3>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+        <h3>statistics</h3>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value = {good} />
+            <StatisticLine text="neutral" value = {neutral} />
+            <StatisticLine text="bad" value = {bad} />
+            <StatisticLine text="average" value = {(good-bad)/total} />
+            <StatisticLine text="positive" value = {good/total*100 + ' %'} />
+          </tbody>
+        </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h3>give feedback</h3>
+      <Button onClick={()=>setGood(good + 1)} text = "good" />
+      <Button onClick={()=>setNeutral(neutral + 1)} text = "neutral" />
+      <Button onClick={()=>setBad(bad + 1)} text = "bad" />
+      
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+      
+    </div>
+  )
+}
+
+//napit good neutral bad
+//statistics (summat jokaisesta)
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//export default App;
