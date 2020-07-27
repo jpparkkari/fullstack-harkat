0143416be6fe74ad76fbdba99542bc85023d1b0b
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-07-28 00:23:35 +0300

    4.2 config, logger, mongo modules done

--- viikko4/index.js
+++ viikko4/index.js
@@ -1,22 +1,11 @@
-require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
+const logger = require('./utils/logger')
 const cors = require('cors')
-const mongoose = require('mongoose')
-const mongoUrl = process.env.MONGODB_URI
+const config = require('./utils/config')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-//const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
@@ -39,7 +28,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
---
+++ viikko4/models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
+const logger = require('../utils/logger')
+
+logger.info('connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
