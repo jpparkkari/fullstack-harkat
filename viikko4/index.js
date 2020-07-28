d8a0580b3e51584fe7a3f7f78b5aff7a19d4c570
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-07-28 09:25:31 +0300

    4.2 blogRouter working

--- viikko4/index.js
+++ viikko4/index.js
@@ -4,29 +4,14 @@ const app = express()
 const logger = require('./utils/logger')
 const cors = require('cors')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = require('./models/blog')
+//const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
