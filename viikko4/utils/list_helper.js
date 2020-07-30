3d8a63c6cb0f307db1f685bdf39e1f45ef99c4c2
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-07-31 00:00:39 +0300

    4.5

--- viikko4/utils/list_helper.js
+++ viikko4/utils/list_helper.js
@@ -2,10 +2,6 @@ const dummy = (blogs) => {
   return 1
 }
 
-
-
-
-
 const totalLikes = (blogs) => {
   const reducer = (acc, data) => {
     return acc + data.likes
@@ -14,7 +10,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const maxBlog = blogs.reduce(
+    (maxBlog, blog) => ( blog.likes > maxBlog.likes ? blog : maxBlog), blogs[0]
+  )
+  const result = `{"title": ${maxBlog.title}, "author": ${maxBlog.author}, "likes": ${maxBlog.likes}}`
+  console.log(result)
+  return result
+  //console.log(maxBlog)
+  //return maxBlog.likes
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
