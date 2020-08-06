99b2c5f4d474ae65b0b1e9d932b50d233fb8cf3b
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-08-06 14:40:59 +0300

    4.10

--- viikko4/tests/blog_api.test.js
+++ viikko4/tests/blog_api.test.js
@@ -4,6 +4,8 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+//const { response } = require('express')
 
 const blogs = [ { _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0 }, { _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0 }, { _id: '5a422b3a1b54a676234d17f9', title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12, __v: 0 }, { _id: '5a422b891b54a676234d17fa', title: 'First class tests', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', likes: 10, __v: 0 }, { _id: '5a422ba71b54a676234d17fb', title: 'TDD harms architecture', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', likes: 0, __v: 0 }, { _id: '5a422bc61b54a676234d17fc', title: 'Type wars', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', likes: 2, __v: 0 }
 ]
@@ -11,6 +13,7 @@ const blogs = [ { _id: '5a422a851b54a676234d17f7', title: 'React patterns', auth
 beforeEach(async () => {
   await Blog.deleteMany({})
   await Blog.insertMany(blogs)
+  logger.info('blogs initialized')
 })
 
 test('there are right amount of blogs', async () => {
@@ -18,6 +21,35 @@ test('there are right amount of blogs', async () => {
 
   expect(response.body).toHaveLength(blogs.length)
 })
+
+test('blogs return id field', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
+test('blogs can be added', async () => {
+  const newBlog = {
+    title: 'test',
+    author: 'tester',
+    url: 'www',
+    likes: 1
+  }
+  
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+
+  //const contents = blogsAtEnd.map(r => r.content)
+  
+  expect(Object.keys(blogsAtEnd.body)).toHaveLength(Object.keys(blogs).length + 1)
+  //console.info('number of records ', Object.keys(blogs).length)
+  //console.info('number of blogs at the end ', Object.keys(blogsAtEnd.body).length)  
+})
+
 
 afterAll(() => {
   mongoose.connection.close()
