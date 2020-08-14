4e01c24f6ffff7e2553d8b941f028f0ce7a9399c
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-08-14 18:23:43 +0300

    4.19

--- viikko4/controllers/blogs.js
+++ viikko4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const logger = require('../utils/logger')
@@ -20,16 +21,26 @@ blogsRouter.get('', (request, response) => {
     })
 })*/
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if(!body.title || !body.url) {
-    response.status(400).end()
-    return
+    return response.status(400).json({ error: 'title or url missing' })
   }
-
-  const user = await User.findOne()//findById(decodedToken.id)
-  logger.info(user)
 
   const blog = new Blog({
     title: body.title,
--- viikko4/utils/middleware.js
+++ viikko4/utils/middleware.js
@@ -4,6 +4,10 @@ const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid or missing token'
+    })
   }
   next(error)
 }
