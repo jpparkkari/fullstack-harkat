ced153f9b746f14e29938a06f66be89c59a28423
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-08-16 11:18:03 +0300

    finalizing round. 4.7 4.8 done

--- viikko4/utils/list_helper.js
+++ viikko4/utils/list_helper.js
@@ -1,3 +1,8 @@
+//const { request } = require("express")
+
+const _ = require('lodash')
+const logger = require("./logger")
+
 const dummy = () => {
   return 1
 }
@@ -21,8 +26,32 @@ const favoriteBlog = (blogs) => {
   //return maxBlog.likes
 }
 
+//4.6 mostBlogs. Gets array of blogs. Returns the author with most blogs and the number of those blogs. Do also tests for this.
+const mostBlogs = (blogs) => {
+ 
+  const count = _.countBy(blogs, 'author')
+  const result = Object.entries(count).sort((a, b) => b[1] - a[1])
+  return { author: result[0][0], blogs: result[0][1] }
+}
+
+//4.7 mostLikes. Gets array of blogs. Returns the author with most likes. Do also tests for this.
+const mostLikes = (blogs) => {
+  const group = _.groupBy(blogs, 'author')
+  const likeCount = _.reduce(group, (result, value, key) => {
+    result[key] = totalLikes(value)
+    return result
+  }, {})
+  const mostLiked = _.find(Object.entries(likeCount).sort((a,b) => b[1]-a[1]))
+
+  return { author: mostLiked[0], likes: mostLiked[1] }
+}
+
+
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
