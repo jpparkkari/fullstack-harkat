f4578c9beea6cdc6ea3e7bed38915262271019c2
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-09-01 22:34:10 +0300

    5.14

--- viikko5/src/components/Blog.test.js
+++ viikko5/src/components/Blog.test.js
@@ -3,10 +3,9 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-describe('<Blog />', () => {
-  let component
 
-  beforeEach(() => {
+
+  test('at start only the author and title are shown', () => {
     const blog = {
       title: 'testtitle',
       author: 'tester',
@@ -15,14 +14,10 @@ describe('<Blog />', () => {
       user: {name: 'noname'}
     }
     const mockHandler = jest.fn()
-    component = render(
-      <Blog blog={blog} handleDelete={mockHandler} handleLikes={mockHandler}/>
+    const mockLikeHandler = jest.fn()
+    const component = render(
+      <Blog blog={blog} handleDelete={mockHandler} handleLikes={mockLikeHandler}/>
     )
-    //component.debug()
-  })
-
-  test('at start only the author and title are shown', () => {
-
     //const div = component.container.querySelector('.togglableContent')
 
     //expect(div).toHaveStyle('display: none')
@@ -39,6 +34,19 @@ describe('<Blog />', () => {
   })
 
   test('clicking the show button shows the other information', () => {
+    const blog = {
+      title: 'testtitle',
+      author: 'tester',
+      url: 'www',
+      likes: 0,
+      user: {name: 'noname'}
+    }
+    const mockHandler = jest.fn()
+    const mockLikeHandler = jest.fn()
+    const component = render(
+      <Blog blog={blog} handleDelete={mockHandler} handleLikes={mockLikeHandler}/>
+    )
+
     const button = component.getByText('view')
     fireEvent.click(button)
 
@@ -50,4 +58,28 @@ describe('<Blog />', () => {
     )
   })
 
-})
+  test('if like button is pressed twice, the event handler gets called two times', () => {
+    const blog2 = {
+      title: 'testtitle2',
+      author: 'tester2',
+      url: 'www2',
+      likes: 3,
+      user: {name: 'noname2'}
+    }
+    const mockHandler2 = jest.fn()
+    const mockLikeHandler2 = jest.fn()
+    const component = render(
+      <Blog blog={blog2} handleDelete={mockHandler2} handleLikes={mockLikeHandler2}/>
+    )
+
+    const showButton = component.getByText('view')
+    fireEvent.click(showButton)
+    
+    const likeButton = component.getByText('like').closest('button')
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+
+    expect(mockLikeHandler2.mock.calls).toHaveLength(2)
+  })
+
+
