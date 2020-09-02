d9140c05b0701131e3763d1ec0aeecacfecf566f
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-09-02 13:58:11 +0300

    5.16

---
+++ viikko5/src/components/Blogform.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent, findAllByTitle } from '@testing-library/react'
+import BlogForm from './BlogForm'
+
+
+test('Correct parameters are returned with props function', () => {
+  const mockHandler = jest.fn()
+  
+  const component = render(
+    <BlogForm createBlog={mockHandler} />
+  )
+
+  const title = component.container.querySelector('#title')
+  const author = component.container.querySelector('#author')
+  const url = component.container.querySelector('#url')
+  const form = component.container.querySelector('form')
+
+  fireEvent.change(title, {
+    target: {value: 'test title'}
+  })
+  fireEvent.change(author, {
+    target: {value: 'tstAuthor'}
+  })
+  fireEvent.change(url, {
+    target: {value: 'www.te.st'}
+  })
+  fireEvent.submit(form)
+
+  console.log(mockHandler.mock.calls[0][0])
+  expect(mockHandler.mock.calls[0][0]).toEqual({ title: 'test title', author: 'tstAuthor', url: 'www.te.st' })
+})
