11eba420d991241742e70ce3834fdabf7440e8db
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-09-08 23:56:37 +0300

    6.8

---
+++ viikko6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, onClick }) => {
+  return (
+    <div>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={onClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+    {anecdotes.map(anecdote =>
+      <Anecdote
+        key={anecdote.id}
+        anecdote={anecdote}
+        onClick={() => dispatch(voteAnecdote(anecdote.id))}
+      />
+    )}
+    </>
+  )
+
+}
+
+export default AnecdoteList
