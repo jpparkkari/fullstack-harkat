90eb010837cafa5ea8b7c9751804e811926f2c03
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-09-11 23:27:16 +0300

    6.19

--- viikko6/redux-anecdotes/src/components/Notification.js
+++ viikko6/redux-anecdotes/src/components/Notification.js
@@ -1,21 +1,28 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notifications = (props) => {
+  //const notification = useSelector(state => state.notification)
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
-  if (notification) {
+  if (props.notification) {
     return (
       <div style={style}>
-        {notification}
+        {props.notification}
       </div>
     )
   }
   else return (<></>)
 }
 
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const Notification = connect(mapStateToProps)(Notifications)
 export default Notification
