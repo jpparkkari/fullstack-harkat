2db7d3009deff4182a45df502e639dd7ef54a7aa
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-09-14 15:05:27 +0300

    7.1

--- viikko7/routed-anecdotes/src/App.js
+++ viikko7/routed-anecdotes/src/App.js
@@ -1,17 +1,10 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom"
 
-const Menu = () => {
-  const padding = {
-    paddingRight: 5
-  }
-  return (
-    <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
-    </div>
-  )
-}
+
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
@@ -121,16 +114,34 @@ const App = () => {
 
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
-
+  const padding = {
+    paddingRight: 5
+  }
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Link style={padding} to='/anecdotes'>anecdotes</Link>
+        <Link style={padding} to='/create'>create new</Link>
+        <Link style={padding} to='/about'>about</Link>
+      </div>
+  
+    <Switch>
+      <Route path='/anecdotes'>
+        <AnecdoteList anecdotes={anecdotes} />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>>
+      <Route path='/create'>
+        <CreateNew addNew={addNew} />
+      </Route>
+      <Route path='/'>
+        <AnecdoteList anecdotes={anecdotes} />
+      </Route>
+    </Switch>
+
+    <Footer />
+    </Router>
   )
 }
 
